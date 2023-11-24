99551fcc02905b80ef99a30744a85b59d62e3507
Crowdbotics <team@crowdbotics.com>
2023-11-24 17:07:40 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "steep-brook-44743",
+  url: "https://steep-brook-44743.botics.co",
+  api: "https://steep-brook-44743.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
